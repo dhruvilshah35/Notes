9b49a4f7c120de9f49384a1211756c96519ceba9
Dhruvil <dhruvilshah35@gmail.com>
2019-10-14 12:31:43 -0700

    Basic UI view controllers

---
+++ Notes/NotesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NotesViewController.swift
+//  Notes
+//
+//  Created by Dhruvil on 10/13/19.
+//  Copyright © 2019 Dhruvil. All rights reserved.
+//
+
+import UIKit
+
+class NotesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Dhruvil on 10/13/19.
+//  Copyright © 2019 Dhruvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
